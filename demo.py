c8123f897e21c2bc1a6dc903be29544029421844
tunan <a715166916@qq.com>
2020-05-31 20:12:31 +0800

    学习

--- demo.py
+++ demo.py
@@ -1,1 +1,60 @@
 print('hello world')
+'''
+print(2333)
+print(2.3333)
+print(None)
+print(True,False)
+print({})
+print([])
+
+a = (123,'haha')
+print(a)
+
+name = '图南'
+print('你好，{}'.format(name))
+'''
+#单行注释
+'''
+多行注释1
+多行注释2
+多行注释3
+'''
+'''
+print('你好'+name)
+print('1'+'1')
+print('1+1')
+print(1+1)
+
+b = (12376+6*8564/56)-99
+c = ((12+56)/75*57)
+print(b-c)
+print(2>1)
+
+name = input('请输入你的名字：')
+print('我叫：'+name)
+
+x = float(input('j='))
+y = float(input('k='))
+z = x + y
+print(z)
+
+m = input('请输入字符串：')
+print(len(m))
+'''
+a = ('啊啊','版本','等等','asd',12.33,12.33,12.33,12.33,12.33,True,False)
+#Ture = 1  False = o
+print(a[2])
+print(a.index(12.33))
+print(a.count(12.33))
+print(a.index('啊啊'))
+print(a.count(1))
+print(a.count(0))
+
+图南 = 1
+print(图南)
+
+b = ['啊啊','版本','等等','asd',12.33,12.33,12.33,12.33,12.33,True,False]
+b.append('方法')
+print(b)
+b.insert(3,'jojo')
+print(b)
